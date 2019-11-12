472879fb19d0fa92db6e4ac560ca37d86684a75a
R. S. Doiel <rsdoiel@gmail.com>
2019-11-12 10:38:52 -0800

    Quick Save

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,13 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 cfg = Config()
+# Flask-login expects app.secret_key instead of config object
+app.secret_key = cfg.SECRET_KEY
 
 from flask_login import LoginManager
 
 login = LoginManager(app)
+# Iniatialize the Flask login manager.
+login.init_app(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,30 @@
-from flask import render_template, flash, redirect, url_for, request, escape
-from flask_login import current_user, login_user
+from flask import render_template, flash, redirect, url_for, request, escape, abort
+from flask_login import current_user, login_user, login_required
 from app import app, cfg, login
 from app.forms import LoginForm
 from app.models import User
+from py_dataset import dataset
 
 @login.user_loader
-def load_user(user_id):
+def user_loader(user_id):
     c_name = cfg.ROLES
     if dataset.has_key(c_name, user_id) == False:
         return None
     u = User(c_name)
     if u.get(user_id) == False:
         return None
-    print(f'DEBUG User loaded {u.username} {u.display_name}')
+    print(f'DEBUG user_loader({user_id}) -> {u.username} {u.display_name}')
     return u
 
 @app.route('/')
 @app.route('/index')
 def index():
-    if current_user.is_anonymous:
+    if current_user.is_authenticated:
+        user = {"username": current_user.username, "display_name": current_user.display_name}
+    elif current_user.is_anonymous:
         user = {"username": "anonymous", "display_name": "Anonymous"}
     else:
-        user = {"username": current_user.username, "display_name": current_user.display_name}
+        user = {"username": 'unknown', "display_name": 'unknown'}
     print(f'DEBUG user is {user}')
     posts = [
         {
@@ -35,14 +38,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user = user, posts = posts)
 
+
 @app.route('/login', methods = ["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         print(f'DEBUG current user is {current_user.username}, redirecting')
-        next = request.args.get('next')
-        if not is_safe_url(next):
-            return flask.about(400)
-        return redirect(next or url_for('index'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         c_name = cfg.USERS
@@ -53,13 +54,16 @@ def login():
         if u.get(username) == False or u.check_password(password) == False:
             print(f'DEBUG {username} or password not found in {c_name}')
             flash('Invalid username or password')
-            return flask.about(400)
+            return abort(401)
         login_user(user = u)
-        print(f'DEBUG current user is logged in, {u.username}')
+        print(f'DEBUG current user is successfully logged in, {u.username}')
+        print(f'DEBUG redirecting to /')
         flash('Logged in successfully.')
-        next = request.args.get('next')
-        if not is_safe_url(next):
-            return flask.about(400)
-        return redirect(next or url_for('index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title="Sign in", user = current_user, form=form)
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
