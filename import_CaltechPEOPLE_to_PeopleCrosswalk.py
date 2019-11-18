6bf70db7d338ebe0ab5d36da8c5f082d084d1ccd
R. S. Doiel <rsdoiel@gmail.com>
2019-11-18 13:20:57 -0800

    Quick Save

--- import_CaltechPEOPLE_to_PeopleCrosswalk.py
+++ import_CaltechPEOPLE_to_PeopleCrosswalk.py
@@ -12,14 +12,17 @@ if __name__ == '__main__':
             print(f'WARNING: read failed, {key} in {c_name1}, {err}')
         else:
             obj2 = {}
+            # Initialize an empty object.
+            for attr2 in [ "cl_people_id", "family_name", "given_name", "thesis_id", "authors_id", "archivesspace_id", "directory_id", "viaf", "lcnaf", "isni", "wikidata", "snac", "orcid", "image", "educated_at", "caltech", "jpl", "faculty", "alumn", "notes" ]:
+                obj2[attr2] = ''
             for attr1 in obj1:
                 attr2 = attr1.lower().replace(' ', '_').replace('(', '').replace(')', '')
-                if attr2 in [ "cl_people_id", "family_name", "given_name", "thesis_id", "authors_id", "archivesspace_id", "directory_id", "viaf", "lcnaf", "isni", "wikidata", "snac", "orcid", "image", "educated_at", "caltech", "jpl", "faculty", "alumni", "notes" ]:
+                if attr2 in [ "cl_people_id", "family_name", "given_name", "thesis_id", "authors_id", "archivesspace_id", "directory_id", "viaf", "lcnaf", "isni", "wikidata", "snac", "orcid", "image", "educated_at", "caltech", "jpl", "faculty", "alumn", "notes" ]:
                     if attr2 in [ 'caltech', 'alumn', 'jpl', 'faculty' ]:
                         obj2[attr2] = True
-                        if obj2[attr2] == '':
+                        if obj1[attr1] == '':
                             obj2[attr2] = False
-                    else:
+                    elif obj1[attr1]:
                         obj2[attr2] = obj1[attr1]
             if dataset.key_exists(c_name2, key):
                 err = dataset.update(c_name2, key, obj2)
